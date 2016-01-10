dc963702de9abfbb58259107955d175f6b3a2487
Niko <niko.caron90@gmail.com>
2016-01-10 19:33:15 -0400

    Build Tools
    Added the following build tools
    - uglify
    - cssmin
    - htmlmin

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		uglify: {
+			my_target: {
+				files: {
+					'js/app.js': 'beautified/js/app.js',
+					'js/bootstrap.js': 'beautified/js/bootstrap.js',
+					'js/jasny-bootstrap.js': 'beautified/js/jasny-bootstrap.js',
+					'js/knockout.js': 'beautified/js/knockout.js'
+				}
+			}
+		},
+		cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+					'css/bootstrap.css': 'beautified/css/bootstrap.css',
+					'css/bootstrap-theme.css': 'beautified/css/bootstrap-theme.css',
+					'css/jasny-bootstrap.css': 'beautified/css/jasny-bootstrap.css',
+					'css/jasny-offcanvas.css': 'beautified/css/jasny-offcanvas.css',
+					'css/map.css': 'beautified/css/map.css',
+					'css/style.css': 'beautified/css/style.css'
+				}
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: {
+					'index.html': 'beautified/index.html'
+				}
+			}
+		}
+	});
+	grunt.registerTask('default', [
+		'uglify',
+		'cssmin',
+		'htmlmin'
+		]);	
+};
